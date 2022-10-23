a550d06233a1cb9b578e346c4a4bcd1925650810
Lightyoo <105405386+Lightyoo@users.noreply.github.com>
2022-10-23 11:13:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,17 +23,17 @@ def wrap_angle(angle):
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        playerwidth,playerheight = 16,28
-        self.image = pygame.Surface((playerwidth,playerheight))
+        width,height = 16,28
+        self.image = pygame.Surface((width,height))
         self.image.fill(GREEN)
         self.rect = Rect(
-            (screen.get_width()-playerwidth)/2,
-            (screen.get_height()-playerheight)/2,
-            playerwidth,
-            playerheight
+            (WIDTH-width)/2,
+            (HEIGHT-height)/2,
+            width,
+            height
         )
         self.posx,self.posy = 0,0
-        self.speedx,self.speedy = 1,1
+        self.speedx,self.speedy = 2,2
         
     def move(self,direction):
         # move the role
@@ -45,7 +45,30 @@ class Player(pygame.sprite.Sprite):
             self.posx-=self.speedx
         elif direction == "RIGHT":
             self.posx+=self.speedx
-            
+
+class Gun(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        width,height = 20,5
+        self.image = pygame.Surface((width,height))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (screen.get_width()-width)/2,(screen.get_height()-height)/2,
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.width,self.height = 20,20
+        self.image = pygame.Surface((self.width,self.height))
+        self.image.fill(PURPLE)
+        self.rect = self.image.get_rect()
+        self.posx,self.posy = x*20,y*20
+
+    def update(self,player):
+        self.rect.topleft = (
+            (WIDTH-self.width)/2-(player.posx-self.posx),
+            (HEIGHT-self.height)/2-(player.posy-self.posy)
+        )
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -53,20 +76,25 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 PURPLE = (255,0,255)
+WIDTH,HEIGHT = 800,550
 
 pygame.init()
-screen = pygame.display.set_mode((800,550))
-pygame.key.set_repeat(120)
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.key.set_repeat(10)
 fps = pygame.time.Clock()
 
 ######### Groups #########
 group_players = pygame.sprite.Group()
-group_guns = pygame.sprite.Group()
+# group_guns = pygame.sprite.Group()
+group_blocks = pygame.sprite.Group()
 
 ##########################
 
 player = Player()
 group_players.add(player)
+# gun = Gun()
+# group_guns.add(gun)
+group_blocks.add(Block(3,3))
 
 
 while True:
@@ -78,22 +106,22 @@ while True:
         if event.type in (QUIT,):
             pygame.quit()
             sys.exit()
-        elif event.type in (KEYDOWN,):
-            # Move the player
-            if event.key == 119:
-                player.move("UP")
-            elif event.key == 115:
-                player.move("DOWN")
-            elif event.key == 97:
-                player.move("LEFT")
-            elif event.key == 100:
-                player.move("RIGHT")
         elif event.type in (MOUSEBUTTONDOWN,):
             if event.button == 4:
                 pass
             elif event.button == 5:
                 pass
-    
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        player.move("UP")
+    if key[pygame.K_s]:
+        player.move("DOWN")
+    if key[pygame.K_a]:
+        player.move("LEFT")
+    if key[pygame.K_d]:
+        player.move("RIGHT")
+
     screen.blit(
         formtext(f"player-position: {player.posx},{player.posy}"),
         (0,0)
@@ -110,10 +138,16 @@ while True:
         (0,24)
     )
 
+    iftouchwall = pygame.sprite.spritecollide(player,group_blocks,False)
+    screen.blit(
+        formtext(f"touch-wall: {iftouchwall}"),
+        (0,36)
+    )
+
     group_players.draw(screen)
-    group_guns.update(screen)
-    group_guns.draw(screen)
+    group_blocks.update(player)
+    group_blocks.draw(screen)
+    # group_guns.update(screen)
+    # group_guns.draw(screen)
 
     pygame.display.update()
-
-    
